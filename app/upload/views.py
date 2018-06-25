d64602a2d417b3e5b7b98a95e782eadd117929f1
zhangwei <123456789@163.com>
2018-06-25 17:42:15 +0800

    文件上传

--- app/upload/views.py
+++ app/upload/views.py
@@ -16,16 +16,21 @@ save
 
 
 """
+
+
 #
 @upload_blue.route('/img/', methods=['GET', 'POST'])
 def upload_img():
     if request.method == 'POST':
-        image = request.files['img']
-        file_name = images.save(image, name=get_new_image_name(image.filename))
+        files = request.files.getlist('files')
+        urls = []
+        for image in files:
+            file_name = images.save(image, name=get_new_image_name(image.filename))
+            url = images.url(file_name)
+            urls.append(url)
         # 生成可以访问的路径
-        url = images.url(file_name)
         # /static/upload/images/xxx.png
-        return jsonify({'msg': 'success', 'status': 200, 'url': url})
+        return jsonify({'msg': 'success', 'status': 200, 'url': urls})
         # 文件上传对象  字典
     elif request.method == 'GET':
         return render_template('upload.html')
